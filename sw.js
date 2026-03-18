7315279bbe453fe0ec418e520bbcdff4dcbcdd01
Nils Flegel <nilsflegel@Mac-mini-von-Nils.fritz.box>
2026-03-18 20:38:18 +0100

    Initial commit: Seelotsen-Fahrtenbuch PWA

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE = 'seelotse-v4';
+const CORE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './xlsx.min.js',
+  './apple-touch-icon.png',
+  './icon-192.png',
+  './icon-512.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.addAll(CORE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(cached => {
+        if (cached) return cached;
+        return fetch(e.request).then(res => {
+          if (res && res.ok) {
+            const clone = res.clone();
+            caches.open(CACHE).then(c => c.put(e.request, clone));
+          }
+          return res;
+        });
+      })
+      .catch(() => caches.match('./index.html'))
+  );
+});
